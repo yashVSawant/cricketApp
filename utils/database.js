fa9d086d049f14b12fded62b9ea1d583affaa2a8
yashVSawant <yashsawant23910@gmail.com>
2024-07-25 20:33:35 +0530

    modified sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize");//MYSQL_ADDON_URI
 
-const sequelize =  new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    dialect:'mysql',
-    host:process.env.DB_HOST
-})
+// const sequelize =  new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
+//     dialect:'mysql',
+//     host:process.env.DB_HOST
+// })
+
+const sequelize = new Sequelize(process.env.MYSQL_ADDON_URI, {
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 
 module.exports = sequelize;
